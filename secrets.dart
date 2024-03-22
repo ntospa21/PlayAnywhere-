ca9498985e6b1cc59390853069e3726900c55dd6
Pantos, Thomas <tpantos@deloitte.gr>
2024-03-22 23:32:35 +0200

    add env

--- secrets.dart
+++
@@ -1,3 +0,0 @@
-const FB_AND_API_KEY = "AIzaSyCCVGmz50_foWdaI5D7_Z4D7PSMo_xyrjY";
-const FB_IOS_API_KEY = "AIzaSyCpPTIZIkmmfj31TE-qYvhDpwxmnEShRgM";
-const FB_MAC_API_KEY = "AIzaSyCpPTIZIkmmfj31TE-qYvhDpwxmnEShRgM";
